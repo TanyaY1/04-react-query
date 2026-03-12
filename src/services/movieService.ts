9dbbdd122a3c9943a3f7a5839e3c161f0d5f80f3
Tanya <yakovetsta1@gmail.com>
2026-03-12 18:50:16 -0300

    fixed

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
-import type { MoviesResponse } from "../types/movie";
+import type { Movie } from "../types/movie";
 
 const token = import.meta.env.VITE_TMDB_TOKEN;
+
+interface MoviesResponse {
+  results: Movie[];
+  total_pages: number;
+}
 
 export const fetchMovies = async (
   query: string,
